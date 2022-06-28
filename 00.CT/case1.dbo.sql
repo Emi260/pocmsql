2dbcd87df559e40406176698db95de8e21aa27b2
Emi260 <65983384+Emi260@users.noreply.github.com>
2022-06-28 15:37:18 -0300

    commit inicial

---
+++ 00.CT/case1.dbo.sql
@@ -0,0 +1,12 @@
+--liquibase formatted sql
+
+--changeset case:1
+CREATE TABLE Persons (
+    PersonID int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255)
+);
+
+--rollback DROP TABLE Personas;
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# pocmsql
+# pocoracle

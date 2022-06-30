caf59565114f181388ad2a2abd7ecee07463b42d
Emi260 <65983384+Emi260@users.noreply.github.com>
2022-06-30 13:59:42 -0300

    2

--- 00.CT/case1.dbo.sql
+++ 00.CT/case1.dbo.sql
@@ -9,4 +9,15 @@ CREATE TABLE Persons (
     City varchar(255)
 );
 
+--rollback DROP TABLE Persons;
+
+--changeset case:2
+CREATE TABLE Personas (
+    PersonID int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255)
+);
+
 --rollback DROP TABLE Personas;

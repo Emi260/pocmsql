5f492921bce54377a1b3873a34cde562bbca1f6d
Emi260 <65983384+Emi260@users.noreply.github.com>
2022-07-04 16:23:59 -0300

    44

--- 00.CT/case1.dbo.sql
+++ 00.CT/case1.dbo.sql
@@ -21,3 +21,14 @@ CREATE TABLE Personas (
 );
 
 --rollback DROP TABLE Personas;
+
+--changeset case:3
+CREATE TABLE Tipos (
+    PersonID int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255)
+);
+
+--rollback DROP TABLE Tipos;

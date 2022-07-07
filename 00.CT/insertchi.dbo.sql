92110b6955071de7da2e5eaeac29c3272827578e
Emi260 <65983384+Emi260@users.noreply.github.com>
2022-07-07 16:29:47 -0300

    fweg

--- 00.CT/insertchi.dbo.sql
+++ 00.CT/insertchi.dbo.sql
@@ -2,4 +2,4 @@
 
 --changeset chicos:2
 INSERT INTO chicos (PersonID, LastName, FirstName, Address, City)
-VALUES (2, "pspe", "pw", "owo", "war"),(3, "2ppe", "33p", "3oo", "3ar"),(4, "pffpe", "gp", "goo", "gar"),(5, "p4pe", "ep", "eoo", "ear"),(6, "pbpe", "sdp", "ewfoo", "fewar"),(7, "pvfpe", "reap", "aergoo", "agear");
+VALUES (2, 'pspe', 'w', 'owo', 'war'),(3, '2ppe', '33p', '3oo', '3ar'),(4, 'pffpe', 'gp', 'goo', 'gar'),(5, 'p4pe', 'ep', 'eoo', 'ear'),(6, 'pbpe', 'sdp', 'ewfoo', 'fewar'),(7, 'pvfpe', 'reap', 'aergoo', 'agear');
